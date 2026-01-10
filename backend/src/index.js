905d17c2d0156315edfa54cd655fbcac4f9842fe
Soumyadipta Seal <s.seal.a.b.c@gmail.com>
2026-01-11 03:56:40 +0530

    done

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser"
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 5001;
 
 // middleware
 app.use(express.json());
+app.use(cookieParser())
 
 // routes
 app.use("/api/auth", authRoutes);
