f1f79d7327310ebd900ad3254f9527adea6f5416
Soumyadipta Seal <s.seal.a.b.c@gmail.com>
2026-01-10 18:19:35 +0530

    fix

---
+++ backend/src/index.js
@@ -0,0 +1,8 @@
+import express from 'express'
+const app= express();
+import authRoutes from './routes/auth.route.js'
+
+app.use('/api/auth',authRoutes);
+app.listen(5001,()=>{
+    console.log("Server is running on port 5001");
+});
