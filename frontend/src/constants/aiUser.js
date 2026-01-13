eb559d76e56d393a3092076590ef7fcebd4a698c
Soumyadipta Seal <s.seal.a.b.c@gmail.com>
2026-01-13 14:00:24 +0530

    name changed

--- frontend/src/constants/aiUser.js
+++ frontend/src/constants/aiUser.js
@@ -1,7 +1,7 @@
 // src/constants/aiUser.js
 export const AI_USER = {
   _id: "ai-chatty",
-  fullName: "Chatty AI 🤖",
+  fullName: "Auron's Chat AI 🤖",
   profilePic: "/ai-avatar.png",
   isAI: true,
 };
