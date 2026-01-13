b54e4e969b5109f811f5c0c9a35bde13f11d6f3f
Soumyadipta Seal <s.seal.a.b.c@gmail.com>
2026-01-14 00:02:15 +0530

    fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -60,12 +60,13 @@ app.use("/api/ai", aiRoutes);
 /* -------------------- PRODUCTION -------------------- */
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+app.get(/.*/, (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../frontend", "dist", "index.html")
+  );
+});
 
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.join(__dirname, "../frontend", "dist", "index.html")
-    );
-  });
 }
 
 /* -------------------- START SERVER -------------------- */
